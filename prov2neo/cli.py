e37e969ae382b2f5819aadbc74259bca77c84639
de Boer, Claas <claas.deboer@dlr.de>
2020-07-01 11:08:58 +0200

    Add command line script entry point.

--- prov2neo/cli.py
+++ prov2neo/cli.py
@@ -4,27 +4,25 @@ import sys
 
 from prov.model import ProvDocument
 
-from core import Importer
-
-
-def parse_args():
-    parser = argparse.ArgumentParser("prov2neo", description="Import W3C PROV graphs to neo4j.")
-    parser.add_argument("-f", "--format", help="input prov format", choices=["provn", "json", "rdf", "xml"], default="json")
-    parser.add_argument("-i", "--input", help="input destination, read from input if none given", default="")
-    parser.add_argument("-H", "--host", help="neo4j instance host")
-    parser.add_argument("-u", "--username", help="neo4j instance username")
-    parser.add_argument("-p", "--password", help="neo4j instance password")
-    return parser.parse_args()
+from .core import Importer
 
 
 def main():
-    args = parse_args()
-    graph, infile = None, None
-    if os.path.exists(args.input):
-        infile = args.input
-    elif not args.input:
-        infile = sys.stdin
+    "Command line script entry point."
+    parser = argparse.ArgumentParser("prov2neo", description="Import W3C PROV graphs to neo4j.")
+    parser.add_argument("-f", "--format", help="input prov format", choices=["provn", "json", "rdf", "xml"], default="json")
+    parser.add_argument("-i", "--input", help="input destination, read from input if none given", required=True)
+    parser.add_argument("-H", "--host", help="neo4j instance host")
+    parser.add_argument("-u", "--username", help="neo4j instance username")
+    parser.add_argument("-p", "--password", help="neo4j instance password")
 
+    args = parser.parse_args()
+    graph, infile = None, None
+    if args.input == ".":
+        infile = sys.stdin
+    elif os.path.exists(args.input):
+        infile = args.input
+    
     graph = ProvDocument.deserialize(source=infile, format=args.format)
     auth = {"host": f"{args.host}", "username": f"{args.username}", "password": f"{args.password}"}
 
