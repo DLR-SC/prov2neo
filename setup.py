cf628282f5122acc0ab99f94d86a48c03a6e46d3
Andreas Schreiber <andreas.schreiber@dlr.de>
2021-03-03 20:59:19 +0100

    Adds PyPI classifiers.

--- setup.py
+++ setup.py
@@ -14,5 +14,20 @@ setup(
     keywords=['w3c prov', 'neo4j', 'graph import'],
     url='https://gitlab.dlr.de/provenance/prov2neo',
     install_requires=['py2neo==2020.1.0', 'prov==2.0.0', 'neotime==1.7.4'],
-    entry_points={'console_scripts': ['prov2neo = prov2neo.cli:main']}
+    entry_points={'console_scripts': ['prov2neo = prov2neo.cli:main']},
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        ],
+    include_package_data=True,
+
 )
