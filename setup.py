548bf56494b4f3814178b0e249911acd6da94865
Andreas Schreiber <andreas.schreiber@dlr.de>
2021-03-03 20:54:39 +0100

    Fixes packages.

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="prov2neo",
-    version="0.1",
-    author="Claas de Boer",
-    author_email="claas.deboer@dlr.de",
-    packages=find_packages(),
-    description="Import W3C PROV graphs into Neo4j using py2neo's OGM.",
-    keywords=["w3c prov", "neo4j", "graph import"],
-    install_requires=["py2neo==2020.1.0", "prov==2.0.0", "neotime==1.7.4"],
-    entry_points={"console_scripts": ["prov2neo = prov2neo.cli:main"]}
+    name='prov2neo',
+    version='0.2',
+    author='Claas de Boer',
+    author_email='claas.deboer@dlr.de',
+    packages=['prov2neo'],
+    description='Import W3C PROV graphs into Neo4j using py2neo\'s OGM.',
+    keywords=['w3c prov', 'neo4j', 'graph import'],
+    url='https://gitlab.dlr.de/provenance/prov2neo',
+    install_requires=['py2neo==2020.1.0', 'prov==2.0.0', 'neotime==1.7.4'],
+    entry_points={'console_scripts': ['prov2neo = prov2neo.cli:main']}
 )
