2ff749cfc8748fde6740853c6996b99f55694ae2
debo_cl <claas.deboer@dlr.de>
2021-06-20 13:17:42 +0200

    Allow Client import from top-level package.

--- prov2neo/__init__.py
+++ prov2neo/__init__.py
@@ -1,5 +1,7 @@
 """Import W3C PROV graphs into Neo4j using py2neo\'s OGM."""
 
+from .client import Client
+
 __author__ = "Claas de Boer"
 __copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
 __license__ = "MIT"
