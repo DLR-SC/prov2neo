d2863346b942962e3ef0ddcb6a91dbb96e9cf554
cdboer <cdboer@cdboer.de>
2022-07-26 17:17:11 +0200

    Increase version number to 1.2.1

--- prov2neo/__init__.py
+++ prov2neo/__init__.py
@@ -5,5 +5,5 @@ from .client import Client
 __author__ = "Claas de Boer"
 __copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
 __license__ = "MIT"
-__version__ = "1.2"
+__version__ = "1.2.1"
 __status__ = "Production"
