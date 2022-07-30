c8f435e8bab2a5b639bb0a089dfded30711b5db0
cdboer <cdboer@cdboer.de>
2022-07-30 15:27:13 +0200

    Implement new command line interface using click

--- prov2neo/cli.py
+++ prov2neo/cli.py
@@ -1,126 +1,103 @@
-import argparse
-import os
-import sys
+import click
 
-from py2neo import ClientError, DatabaseError
-from prov.model import ProvDocument
-
-from prov2neo.client import Client, ConnectionError
+from prov2neo import __version__
+from prov2neo import exceptions as exc
+from prov2neo.client import SUPPORTED_SCHEMES, Client
+from prov2neo.utils import read_doc, write_doc
 
 
-def create_parser():
-    """Create the command line interface argument parser.
-
-    Params:
-    -------
-    """
-    parser = argparse.ArgumentParser("prov2neo",
-                                     description="Import W3C PROV documents to Neo4j.")
-    parser.add_argument("-f", "--format",
-                        help="input PROV format",
-                        choices=["provn", "json", "rdf", "xml"],
-                        default="json",)
-    parser.add_argument("-i", "--input",
-                        help="input files, use '.' for stdin",
-                        nargs="+", default=None)
-    parser.add_argument("-a", "--address",
-                        help="Neo4j address")
-    parser.add_argument("-u", "--username",
-                        help="Neo4j username")
-    parser.add_argument("-p", "--password",
-                        help="Neo4j password")
-    parser.add_argument("-n", "--name",
-                        help="Neo4j target database name", default="neo4j")
-    parser.add_argument("-s", "--scheme",
-                        help="connection scheme to use when connecting to Neo4j",
-                        choices=["bolt", "bolt+s", "bolt+ssc",
-                                 "http", "https", "http+s", "http+ssc"],
-                        default="bolt+s")
-    return parser
+@click.group(chain=True)
+@click.version_option(version=__version__, prog_name="prov2neo")
+@click.pass_context
+def main(ctx: click.Context):
+    """Connect to a neo4j instance and import/export provenance documents."""
+    ctx.obj = Client()
 
 
-def deserialize(file_path, fmt):
-    """Deserialize a provenance graph from a file."""
-    return ProvDocument.deserialize(source=file_path, format=fmt)
+@main.command()
+@click.option("-a", "--address", required=True, type=str, help="Neo4j address.")
+@click.option("-u", "--user", required=True, type=str, help="Neo4j account username.")
+@click.option(
+    "-p", "--password", required=True, type=str, help="Neo4j account password."
+)
+@click.option(
+    "-n",
+    "--dbname",
+    default="neo4j",
+    show_default=True,
+    type=str,
+    help="Neo4j database name.",
+)
+@click.option(
+    "-s",
+    "--scheme",
+    default="bolt",
+    show_default=True,
+    type=click.Choice(SUPPORTED_SCHEMES),
+    help="Connection scheme.",
+)
+@click.pass_obj
+@exc.on_database_error(
+    hint=f"Check if your neo4j version supports the creation of new databases."
+    f"(The community edition does not!)",
+)
+@exc.on_client_error(msg="Client side error")
+@exc.on_connection_error(
+    hint="Check if you specified the correct database address, username and password."
+)
+@exc.on_value_error(hint="Check if you specified a supported connection scheme.")
+def connect(
+    client: Client,
+    address: str,
+    user: str,
+    password: str,
+    dbname: str,
+    scheme: str,
+):
+    """Connect to a neo4j instance."""
+    client.connect(
+        address=address,
+        user=user,
+        password=password,
+        dbname=dbname,
+        scheme=scheme,
+    )
 
 
-def create_and_connect_client(args):
-    """Create a `prov2neo` client and connect it to a neo4j instance."""
-    client = Client()
-    client.connect(args.address, args.username, args.password, args.name, args.scheme)
-    return client
+@main.command("import")
+@click.option(
+    "-i",
+    "--input",
+    multiple=True,
+    required=True,
+    type=click.Path(exists=True),
+    help="PROV file[s] to import.",
+)
+@click.pass_obj
+def import_doc(client: Client, input: list[str]):
+    """Import a provenance document into neo4j."""
+    if not client.is_connected:
+        ctx = click.get_current_context()
+        ctx.fail("Import failed: Not connected to a neo4j instance.")
+    for fp in input:
+        graph = read_doc(fp)
+        client.import_doc(graph)
 
 
-def main():
-    """Command line script entry point.
-
-    Retrieve command line arguments and proceed from there.
-    When no input is given or the paths to the input files
-    are faulty, the program prints the --help message
-    and exits.
-
-    If the paths to the input file are valid the files are
-    deserialized into a prov.model.ProvDocuments.
-    A prov2neo.client.Client is created and a connection to
-    a neo4j instance is established before importing the
-    ProvDocument/ProvDocuments into the neo4j instance.
-
-    Params:
-    -------
-    """
-    parser = create_parser()
-    args = parser.parse_args()
-
-    if not args.input:
-        print(parser.format_help())
-        return
-
-    # remove duplicates
-    args.input = list(set(args.input))
-
-    # do not read from stdin, when importing multiple graphs
-    if len(args.input) > 1 and "." in args.input:
-        args.input = [fp for fp in args.input if fp != "."]
-
-    try:
-        client = create_and_connect_client(args)
-    except DatabaseError as e:
-        print(f"{str(e)}")
-        print("Hint: Check if your Neo4j version supports the creation of new databases. (The Community Edition does not!)")
-        return
-    except ClientError as e:
-        print(f"{str(e)}")
-        return
-    except ConnectionError as e:
-        print(f"{str(e)}")
-        print("Hint: Check if you specified the correct database address, username and password.")
-        return
-    except ValueError as e:
-        print(f"{str(e)}")
-        print("Hint: Check if you specified a supported connection scheme.")
-        return
-
-    if args.input == ["."]:
-        try:
-            graph = deserialize(sys.stdin, args.format)
-        except:
-            print(f"Failed to deserialize from stdin.")
-            return
-        client.import_graph(graph)
-    else:
-        # check for valid file paths before deserialization/import
-        for fp in args.input:
-            if not os.path.exists(fp):
-                print(f"File {fp} not found. Invalid path?")
-                return
-        # deserialize and import one file at a time
-        for fp in args.input:
-            try:
-                graph = deserialize(fp, args.format)
-            except:
-                print(f"Failed to deserialize {fp}.")
-                return
-            client.import_graph(graph)
+@main.command("export", deprecated=True)
+@click.option("-o", "--output", type=click.Path(), help="File to export to.")
+@click.option(
+    "-f", "--format", type=click.Choice(["json", "xml", "rdf", "dot", "provn"])
+)
+@click.pass_obj
+def export_doc(client: Client, output: str, format: str):
+    """Export a provenance document from neo4j."""
+    if not client.is_connected:
+        ctx = click.get_current_context()
+        ctx.fail("Export failed: Not connected to a neo4j instance.")
+    # export in multiple formats...
+    graph = client.export_doc()
+    write_doc(graph, output, format)
 
 
 if __name__ == "__main__":
