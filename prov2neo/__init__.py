2a22bcc3be4a969454e873e02a05c2c1de14cfdf
cdboer <cdboer@cdboer.de>
2022-07-30 15:26:27 +0200

    Increase version number to 1.3

--- prov2neo/__init__.py
+++ prov2neo/__init__.py
@@ -1,9 +1,11 @@
-"""Import W3C PROV graphs into Neo4j using py2neo\'s OGM."""
+"""Import W3C PROV documents into Neo4j using py2neo's OGM."""
 
 from .client import Client
+from .utils import read_doc, write_doc
+
 
 __author__ = "Claas de Boer"
 __copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
 __license__ = "MIT"
-__version__ = "1.2.2"
+__version__ = "1.3"
 __status__ = "Production"
