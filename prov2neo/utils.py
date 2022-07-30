3154e3c906aeaac06dc26816b6dd1a86853290af
cdboer <cdboer@cdboer.de>
2022-07-30 15:24:01 +0200

    Add helpers to read and write provenance documents

---
+++ prov2neo/utils.py
@@ -0,0 +1,15 @@
+from prov.model import ProvDocument
+
+
+def read_doc(filepath):
+    for format in ["json", "rdf", "xml"]:
+        try:
+            return ProvDocument.deserialize(source=filepath, format=format)
+        except:
+            continue
+    raise Exception
+
+
+def write_doc(doc, filepath, format):
+    raise NotImplementedError
+    
